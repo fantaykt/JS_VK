cd017e733595a3b7c49a377fde42de4c4a355e1d
vasilyeverstov <everstov.v@gmail.com>
2022-06-15 00:54:07 +0900

    new ficha

--- js_homework_hard3.js
+++ js_homework_hard3.js
@@ -5,16 +5,23 @@ var age_2 = 18
 var age_3 = 60
 
 function showMessage(age_1) {
-    if (parseInt(age_1) || typeof(age_1)=='number') {   //если строка цифры или тип age number
+    age_1 = parseInt(age_1)
+   
+    if (typeof(age_1)=='number' & isNaN(age_1)==false) {  
         if (age_1 < age_2) {console.log("You don't have access cause your age is " + age_1 + ". It's less then "+age_2)} 
         else if ((age_1>=age_2&age_1<age_3)) {console.log("Welcome!")} 
         else if (age_1>age_3) {console.log("Keep calm and look Culture channel")}
-        else {console.log("Technical work")} }
+        else {console.log("Technical work" + typeof(age_1) + " " + age_1)} }
+   
     else {console.log("Error  - not number")}
 
 }
-showMessage(17);
+/*showMessage(17);
 showMessage(18);
 showMessage(61);
 showMessage("25");
+showMessage("25ff");
 showMessage("test");
+*/
+showMessage(true);
+showMessage(false);
