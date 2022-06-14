3f9f7501768c9ab6b925b5823dbb374c73250529
vasilyeverstov <everstov.v@gmail.com>
2022-06-15 01:09:48 +0900

    add regulars

--- js_homework_hard3.js
+++ js_homework_hard3.js
@@ -5,9 +5,8 @@ var age_2 = 18
 var age_3 = 60
 
 function showMessage(age_1) {
-    age_1 = parseInt(age_1)
-   
-    if (typeof(age_1)=='number' & isNaN(age_1)==false) {  
+    if ((/^[-+]?(\d+)$/.test(age_1))){age_1 = parseInt(age_1)}
+        if (typeof(age_1)=='number' & isNaN(age_1)==false) {  
         if (age_1 < age_2) {console.log("You don't have access cause your age is " + age_1 + ". It's less then "+age_2)} 
         else if ((age_1>=age_2&age_1<age_3)) {console.log("Welcome!")} 
         else if (age_1>age_3) {console.log("Keep calm and look Culture channel")}
@@ -16,12 +15,9 @@ function showMessage(age_1) {
     else {console.log("Error  - not number")}
 
 }
-/*showMessage(17);
+showMessage(17);
 showMessage(18);
 showMessage(61);
 showMessage("25");
 showMessage("25ff");
 showMessage("test");
-*/
-showMessage(true);
-showMessage(false);
