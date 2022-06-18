4b703e8f86611a68d99465fa6a71b58cb6703337
vasilyeverstov <everstov.v@gmail.com>
2022-06-18 20:52:15 +0900

    izmenenia

--- js_homework2.2hard.js
+++ js_homework2.2hard.js
@@ -1,4 +1,5 @@
 function printSmile(stroka, numberOfRows){
+    stroka = stroka.toString()
     for (let i = 1 ; i <=numberOfRows ; i++ ) { console.log(stroka.repeat(i))};
     }
     printSmile(2, 5)
